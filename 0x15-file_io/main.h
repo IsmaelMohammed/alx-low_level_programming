6b80521a27dec59657235824abf0b3c2f1527309
Ismael Mohammed <ismaelmohammed97@gmail.com>
2022-08-22 17:53:20 +0300

    a program that displays the info contained in the ELF header

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -8,4 +8,5 @@
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
+int _strncmp(const char *s1, const char *s2, size_t n)
 #endif
